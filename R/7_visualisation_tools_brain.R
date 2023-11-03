302e5021ea973a9c6d16448d30679447d0a0f18a
FRramon <francois_ramon@orange.fr>
2023-11-03 14:14:43 +0100

    Add visualization plot update documentation package

--- R/7_visualisation_tools_brain.R
+++ R/7_visualisation_tools_brain.R
@@ -2,8 +2,7 @@
 
 #' Idea : Visualisation tool on a Desterieux atlas using ggseg or ggseg3d.
 #' Would just need a table of values by zones (like degree of each node, col1 = region_label, col2 = value)
-#'
-#'
+
 
 library(ggseg)
 library(ggseg3d)
@@ -11,11 +10,15 @@ library(dplyr)
 library(ggsegDesterieux)
 library(ggplot2)
 
-cortical_labels <- brain_labels(desterieux)
-subcortical_labels <- brain_labels(aseg)
+#cortical_labels <- brain_labels(desterieux)
+#subcortical_labels <- brain_labels(aseg)
 
-#Plot Desterieux (freesurfer) cortical representation in 2d with labels
-#' import
+#' Plot Desterieux (freesurfer) cortical atals in 2d with labels
+#'
+#' @return
+#' @export
+#'
+#' @examples
 plot_cortdesterieux2d <- function(){
   plot(desterieux) +
     theme(legend.position = "bottom",
@@ -24,14 +27,35 @@ plot_cortdesterieux2d <- function(){
 
 }
 
-#' import
+#' Plot destrieux atlas in 3d.
+#'
+#' @return
+#' @export
+#'
+#' @examples
 plot_cortdesterieux3d <- function(){
   ggseg3d(atlas = desterieux_3d) %>%
     pan_camera("right lateral")
-
 }
 
-
+#' plot subcortical atlas.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_subcort <- function(){
+  plot(aseg) +
+    theme(legend.position = "bottom",
+          legend.text = element_text(size = 7),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.background = element_blank(),
+          axis.line = element_blank(),
+          axis.text =  element_blank(),
+          axis.ticks = element_blank()) +
+    guides(fill = guide_legend(ncol = 3))
+}
 
 
 dataOnBrain = data.frame(
@@ -40,8 +64,24 @@ dataOnBrain = data.frame(
   stringsAsFactors = FALSE)
 
 #Make the plot of the cortical region colored by there value of p (which can be either degree, betweenness etc etc)
+#' Plot data on brain on an indiviudal level. On destrieux atlas. from data giving each cortical region value.
+#'
+#' @param dataOnBrain
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_dataonbrain <- function(dataOnBrain){
+  dataOnBrain %>%
+    brain_join(desterieux, by = "label") %>%
+    ggplot() +
+    geom_sf(aes(fill = p)) +
+    theme(panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.background = element_blank(),
+          axis.line = element_blank(),
+          axis.text =  element_blank(),
+          axis.ticks = element_blank())
 
-dataOnBrain %>%
-  brain_join(desterieux, by = "label") %>%
-  ggplot() +
-  geom_sf(aes(fill = p))
+}
